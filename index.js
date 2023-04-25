b0b72cc19e03acaa0946972125d52bbdec12ca30
PP Nuttapan <88316947+Rapizz1112@users.noreply.github.com>
2023-04-25 15:51:49 +0700

    Node

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({port: 8080}, () => {
+    console.log('server started');
 })
-app.listen(process.env.PORT || 3000)
+
+wss.on('connection', (ws) => {
+    ws.send("Connect")
+    ws.on('message',(data)=> {
+        const dataString = String(data);
+        console.log('data received \n' + data)     
+        switch(dataString)
+        {
+            case 'Hello':
+                {
+                    ws.send("Hello too")
+                    break;
+                }
+            case 'Hello2':
+                {
+                    ws.send("Hello2 too")
+                    break;
+                }
+            default :
+            {
+                ws.send("error")
+                break;
+            }
+        }
+    })
+})
+
+wss.on('listening', () => {
+    console.log('server is listening on port 8080')
+})
+function broadcastUpdate () {
+    wss.clients.forEach(function each (client) {
+      // filter disconnected clients
+      if (client.readyState !== WebSocket.OPEN) return
+      // filter out current player by client.udid
+      var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid)
+      // create array from the rest
+      var otherPlayersPositions = otherPlayers.map(udid => players[udid])
+      // send it
+      client.send("Hello");
+    })
+   
+  }
